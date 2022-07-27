2b35ddbcfa49dce850eace5bc9c451bc30a2ff7e
B-smd <djaja@iinet.net.au>
2022-07-27 11:14:41 +0800

    add front end & update backend

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const storyRoutes = require('./story-routes');
-
-router.use('/users', userRoutes);
-router.use('/stories', storyRoutes);
-
-module.exports = router;
