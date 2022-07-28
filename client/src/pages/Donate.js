43e7af0d7ba25fd3fe8c746e078536152658f521
B-smd <djaja@iinet.net.au>
2022-07-28 18:18:36 +0800

    Update to package.json

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Donate = () => {
     return (
-        <div className= "display-flex justify-content-space-between donation">
+        <div className= "justify-content-center donation">
         <h2  style= {{color: "aliceblue"}}>Donation</h2>
         <ul>All donation will go to: Caritas, Catholic Mission and LifeLink</ul>
         <a className="btn btn-lg btn-danger m-2" href="https://www.caritas.org.au/" style={{"margin": "auto"}}>Caritas Australia</a>
